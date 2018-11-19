2c1984bafedc9564a1ec7f2b6ae0bb8016392660
Danielsv12 <40643810+Danielsv12@users.noreply.github.com>
2018-11-19 09:00:40 -0600

    Update RedNeuronal.cpp

--- RedNeuronal.cpp
+++ RedNeuronal.cpp
@@ -1,11 +1,1 @@
-#ifndef REDNEURONAL_H
-#define REDNEURONAL_H
 
-class RedNeuronal{
-	lista Capas ;
-	RedNeuronal() ;
-	AgregarCapa(int cNeuronas) ;
-	Categoría clasificar(dato) ;
-	entrenar(datos) ;
-};
-#endif
