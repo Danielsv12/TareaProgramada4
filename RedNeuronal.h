af7381f4c00e463a8f5e2ff14dca835b2056f278
Hugh Mungus <horskom15@gmail.com>
2018-11-19 08:59:45 -0600

    Update RedNeuronal.h

--- RedNeuronal.h
+++ RedNeuronal.h
@@ -1,1 +1,11 @@
+#ifndef REDNEURONAL_H
+#define REDNEURONAL_H
 
+class RedNeuronal{
+	lista Capas ;
+	RedNeuronal() ;
+	AgregarCapa(int cNeuronas) ;
+	Categoría clasificar(dato) ;
+	entrenar(datos) ;
+};
+#endif
