0d854e4527c29c6f751f8ed76ff574eb67a0f539
Danielsv12 <40643810+Danielsv12@users.noreply.github.com>
2018-11-26 08:53:21 -0600

    Update Neurona.h

--- Neurona.h
+++ Neurona.h
@@ -1,14 +1,23 @@
 #ifndef NEURONA_H
 #define NEURONA_H
 #include "Lista.h"
+#include "Conexion.h"
 class Neurona{
 
 float umbral;
 float carga;
   Lista<Conexiones>;
+  Neurona * siguiente;
   
+public:
+   {
+Neurona( float u , float c){}     
+        umbral= u ;
+        carga = c;
+        siguiente= nullptr;
+    }
 
-
+  
 
 
 
