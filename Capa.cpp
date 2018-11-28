830ca4e2dccbc44873484e338dc5ed69f5756d06
Danielsv12 <40643810+Danielsv12@users.noreply.github.com>
2018-11-27 21:34:51 -0600

    Update Capa.cpp

--- Capa.cpp
+++ Capa.cpp
@@ -1,10 +1,15 @@
-#include "capa.h"
+ #include "capa.h"
+
 Capa::Capa(int numerodeneuronas ){
 
     for(int o = 0; o < numerodeneuronas; o++){
         Neurona *nuevan = new Neurona();
         neuronas->Insertar(nuevan);
-
-
     }
 }
+
+ void Capa::CambioDeCarga(float carga, int x){
+
+        neuronas->getI(x)->SetCarga(carga);
+    }
+
