93bf7230ee66d726adabee4ace73b4d1fd60f7cc
Danielsv12 <40643810+Danielsv12@users.noreply.github.com>
2018-11-27 21:03:22 -0600

    Update Capa.cpp

--- Capa.cpp
+++ Capa.cpp
@@ -1,1 +1,10 @@
+#include "capa.h"
+Capa::Capa(int numerodeneuronas ){
 
+    for(int o = 0; o < numerodeneuronas; o++){
+        Neurona *nuevan = new Neurona();
+        neuronas->Insertar(nuevan);
+
+
+    }
+}
